5ed862bb7530e01a504dc60e60797663a6696525
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-12-13 23:51:56 -0300

    singleton run example implemented

--- src/Singleton/index.js
+++ src/Singleton/index.js
@@ -2,22 +2,28 @@ const logger = require('./Logger'),
   Shopper = require('./Shopper'),
   Store = require('./Store');
 
-logger.log('Starting app...');
+const runExampĺe = () => {
+  
+  logger.log('Starting app...');
+  
+  new Shopper('Osman', 500);
+  new Store('Steep and Deep Suplies', [
+    {
+      item: 'Downhill Skis',
+      qty: 1,
+      price: 750
+    },
+    {
+      item: 'Knit Hat',
+      qty: 1,
+      price: 5
+    }
+  ]);
+  
+  logger.log('Finished config...')
+  
+  logger.log(`${logger.count} logs total.`);
 
-const Osman = new Shopper('Osman', 500);
-const skiShop = new Store('Steep and Deep Suplies', [
-  {
-    item: 'Downhill Skis',
-    qty: 1,
-    price: 750
-  },
-  {
-    item: 'Knit Hat',
-    qty: 1,
-    price: 5
-  }
-]);
+}
 
-logger.log('Finished config...')
-
-logger.log(`${logger.count} logs total.`);
+module.exports = { runExampĺe }
