384e82995bb12fc1dd07ce09a68cc97106372dc1
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-12-14 22:26:28 -0300

    decorators inventory item classes implemented

---
+++ src/Decorator/InventoryItem.js
@@ -0,0 +1,35 @@
+class InventoryItem {
+
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  print() {
+    console.log(`${this.name} costs ${this.price}.`)
+  }
+
+}
+
+class GoldenInventoryItem {
+  
+  constructor(baseItem) {
+    this.name = `Golden ${baseItem.name}`;
+    this.price = 1000 + baseItem.price;
+  }
+
+}
+
+class DiamondInventoryItem {
+  
+  constructor(baseItem) {
+    this.name = `Diamond ${baseItem.name}`;
+    this.price = 2000 + baseItem.price;
+    this.cutGlass = true;
+  }
+
+  print() {
+    console.log(`${this.name} costs a lot of money!`);
+  }
+
+}
