dfcd3dd0736209ab1061fbace3e13be0a3529c6d
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-12-14 22:58:24 -0300

    decorator example implemented

--- src/Decorator/index.js
+++ src/Decorator/index.js
@@ -6,5 +6,25 @@ const Shopper = require('./Shopper'),
   } = require('./InventoryItem');
 
 const runExampĺe = () => {
-  
+  const adventurer = new Shopper('Osman', 5000);
+  const items = {
+    sword: new InventoryItem("Sword", 250),
+    cuirass: new InventoryItem("Cuirass", 150)
+  };
+
+  //After a loot, the adventurer finds a legendary blacksmith who improves their items!
+  const enhancedItems = {
+    goldenCuirass: new GoldenInventoryItem(items.cuirass),
+    diamondSword: new DiamondInventoryItem(items.sword)
+  };
+
+  adventurer.purchase(enhancedItems.goldenCuirass);
+  adventurer.purchase(enhancedItems.diamondSword);
+
+  adventurer.printStatus();
+
+  enhancedItems.diamondSword.print()
+
 };
+
+module.exports = { runExampĺe };
