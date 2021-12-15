17d0b62c36b3a7d1bb3382c6003915297e2d24c9
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-12-15 20:19:47 -0300

    strategy logger implemented

--- src/Strategy/Logger.js
+++ src/Strategy/Logger.js
@@ -1,7 +1,26 @@
 import config from './config.json';
+import LogStrategy from './LogStrategy';
 
 class Logger {
+  constructor(strategy='toConsole') {
+    this.logs = [];
+    this.strategy = LogStrategy[strategy];
+  }
 
+  get count() {
+    return this.logs.length;
+  }
+
+  changeStrategy(newStrategy) {
+    this.strategy = LogStrategy[newStrategy];
+  }
+
+  log(message) {
+    const timestamp = new Date().toISOString();
+    this.logs.push({ message, timestamp });
+    this.strategy(message, timestamp);
+  }
+  
 }
 
 module.exports = new Logger(config.logs.strategy);
