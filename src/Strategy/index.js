c9a287e52cece9f20503c34bc34e2fb009a1b7a9
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-12-15 21:06:43 -0300

    strategy example log adjusts

--- src/Strategy/index.js
+++ src/Strategy/index.js
@@ -1,20 +1,27 @@
 const logger = require('./Logger');
 
 const runExample = () => {
-
-  logger.log('A message from after life...');
-  logger.log('My son,');
-  logger.log('The day you were born,');
-  logger.log('the very forests of Lordaeron whispered the name:');
-  logger.log('"Arthas."');
-  logger.log('My child,');
-  logger.log('I watched you with pride,');
-  logger.log('As you grew into a weapon');
-  logger.log('Of rightousness.');
   
+  logger.log(`
+  ==========
+  A message from after life...
+  ==========
+  My son,
+  The day you were born,
+  The very forests of Lordaeron whispered the name:
+  "Arthas".
+  My child,
+  I watched you with pride,
+  As you grew into a weapon,
+  Of rightousness.
+  ==========
+  End 
+  ==========
+  `);
+
   logger.changeStrategy('toFile');
   
-  logger.log('A message from after life was sent by King Terenas Menethil.');
+  logger.log(`A message from after life was sent by King Terenas Menethil.`);
   logger.log('The destinary is Arthar Menethil.');
   logger.log('They were father and son. Both are fallen kings.');
 
