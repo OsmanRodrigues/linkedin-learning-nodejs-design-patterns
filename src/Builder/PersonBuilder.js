1cfff930248c53a6ed4ec1eff5cf331586b52519
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-12-16 21:33:15 -0300

    builder implemented

--- src/Builder/PersonBuilder.js
+++ src/Builder/PersonBuilder.js
@@ -1,5 +1,34 @@
 class PersonBuilder {
+  constructor(name) {
+    this.name = name;
+  }
 
+  makeEmployee() {
+    this.isEmployee = true;
+    return this;
+  }
+
+  makeManager(hours=40) {
+    this.isManager = true;
+    this.hours = hours;
+    return this;
+  }
+
+  makePartTime(hours=20) {
+    this.hours = hours;
+    return this;
+  }
+
+  withMoney(money) {
+    this.money = money;
+    return this;
+  }
+
+  withShoppingList(list=[]) {
+    this.shoppingList = list;
+    return this;
+  }
+  
 }
 
 module.exports = PersonBuilder;
