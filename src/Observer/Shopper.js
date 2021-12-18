88dd023bb7b5c95f490da0e1575e6f29cf6581fd
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-12-17 22:50:42 -0300

    observer shopper notify increment

--- src/Observer/Shopper.js
+++ src/Observer/Shopper.js
@@ -4,6 +4,12 @@ class Shopper {
     this.name = name;  
   }
 
+  notify(storeName, discount) {
+    console.log(
+      `${this.name}, there is a sale at ${storeName}! ${discount}% off everything!`
+    );
+  }
+
 }
 
 module.exports = Shopper;
