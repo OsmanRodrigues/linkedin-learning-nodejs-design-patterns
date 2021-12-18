87712e5c4cff5bce58d4702589cbac6c1958be0c
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-12-17 22:46:32 -0300

    observer store implemented

--- src/Observer/Store.js
+++ src/Observer/Store.js
@@ -1,5 +1,12 @@
 class Store {
+  
+  constructor(name) {
+    this.name = name;
+  }
 
+  sale(discount) {
+    console.log(`Annouce sale at ${this.name}, ${discount}% off everything!`);
+  }
 }
 
 module.exports = Store;
