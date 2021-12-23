453fb059627f30b46d0df7dcdd905835619b2067
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-12-22 23:05:55 -0300

    factory shopper implemented

--- src/Factory/Employee.js
+++ src/Factory/Employee.js
@@ -1,9 +1,11 @@
 class Employee {
+
   constructor(name, money, employer) {
     this.name = name;
     this.money = money;
     this.employer = employer;
   }
+  
 }
 
 module.exports = Employee;
--- src/Factory/Shopper.js
+++ src/Factory/Shopper.js
@@ -1,5 +1,10 @@
 class Shopper {
   
+  constructor(name, money) {
+    this.name = name;
+    this.money = money;
+  }
+
 }
 
 module.exports = Shopper;
