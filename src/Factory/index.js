d8d75fad7a479085026934b764cada5c0bfac610
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-12-23 00:32:52 -0300

    factory description and name list added

--- src/Factory/index.js
+++ src/Factory/index.js
@@ -1,3 +1,10 @@
+/** The Factory patterns allow us to improve our 
+ * code by encapsulating all of the constructors 
+ * into a single module and creating a function 
+ * that will create the objects for us. It's helpful 
+ * maintain sustainability our applications as they 
+ * grow and change. 
+ **/
 const userFactory = require("./userFactory");
 
 const runExample = () => {
