113b55420add767a1155d9095279b3128072b8c7
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-12-23 23:36:56 -0300

    chain of responsibility description and list name added

--- src/ChainOfResponsibility/index.js
+++ src/ChainOfResponsibility/index.js
@@ -1,3 +1,8 @@
+/** The Chain of responsibility desing pattern allow us to chain 
+ * together objects to handle a request. When a request comes, 
+ * it's received to a handler that can send back a response or 
+ * pass to next object which have the same behavior.  
+ **/
 const inventory = require("./inventory.json");
 const Store = require("./Store");
 
