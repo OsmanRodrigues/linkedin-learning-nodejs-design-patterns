b5141fa7566a99deffb608214f6a9a5dc0f710b4
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-12-28 23:41:40 -0300

    iterator inventory implemented

---
+++ src/Iterator/Inventory.js
@@ -0,0 +1,14 @@
+class Inventory{
+
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  writeLine() {
+    process.stdout.write(`${this.name}: $${this.price}`);
+  }
+
+}
+
+modules.exports = Inventory;
